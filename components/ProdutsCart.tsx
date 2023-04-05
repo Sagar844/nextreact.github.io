f7df8d8a2986da377099199f8f9f2d1717c310e4
SaGaR KuMaR <sagarkumar863081@gmail.com>
2023-04-05 19:18:19 +0530

    cv

--- components/ProdutsCart.tsx
+++ components/ProdutsCart.tsx
@@ -7,18 +7,21 @@ type movieprops = {
   movies: movies;
 };
 
-const posturl = (posterpath:string) => {
+const posturl = (posterpath: string) => {
   return `https://image.tmdb.org/t/p/original${posterpath}`;
-
-}
+};
 
 const ProdutsCart = ({ movies }: movieprops) => {
   return (
     <div>
-      <Link href={'/tranding/'+ movies.id }>
+      <Link href={"/tranding/" + movies.id}>
+        <Image
+          height={100}
+          width={100}
+          alt="ss"
+          src={posturl(movies.poster_path)}
+        ></Image>
         <h1>{movies.title}</h1>
-        <Image height={100} width={100} alt="ss" src={posturl(movies.poster_path)}></Image>
-
       </Link>
     </div>
   );
