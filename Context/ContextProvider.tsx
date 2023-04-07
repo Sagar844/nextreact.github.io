d544bc250695b4c1fa0c191a37fc2ba7c00c54f4
SaGaR KuMaR <sagarkumar863081@gmail.com>
2023-04-07 20:36:27 +0530

    sc

--- Context/ContextProvider.tsx
+++ Context/ContextProvider.tsx
@@ -1,5 +1,5 @@
 import {createContext} from 'react'
 
 
-const CartContext = createContext()
+const CartContext = createContext({})
 export default CartContext
--- components/withprovider.tsx
+++ components/withprovider.tsx
@@ -4,9 +4,7 @@ import { useContext } from "react";
 
 // eslint-disable-next-line react/display-name
 const  withprovider = (provider:any) => (IncomingComponets:any) => (props:any) => {
-
 const  datacontext:any = useContext(provider);
-
 return <IncomingComponets {...props} {...datacontext}/>
 
 }
